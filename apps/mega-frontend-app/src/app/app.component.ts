a192edca6f1a3a338814149b8243fba694bc3cdd
LeitnerMi <michael.leitner@gepardec.com>
2024-01-18 08:57:17 +0100

    [Gepardec/mega#732] Cleaned up imports, which were used for testing

--- apps/mega-frontend-app/src/app/app.component.ts
+++ apps/mega-frontend-app/src/app/app.component.ts
@@ -1,21 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
-import { DatepickerMonthYearComponent } from '../../../../libs/shared/ui/src/lib/datepicker-month-year/datepicker-month-year.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, DatepickerMonthYearComponent],
+  imports: [NxWelcomeComponent, RouterModule],
   selector: 'mega-app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'mega-frontend-app';
-
-  date = new Date();
-
-  logDate(date: Date) {
-    console.log(date);
-  }
 }
